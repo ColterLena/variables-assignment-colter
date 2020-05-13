f88566d32ff183ce06d487f7f7fef75c085121e2
Colter Lena <colterlena@verizon.net>
2020-05-12 21:28:07 -0400

    Done with Explorer Mode

--- Program.cs
+++ Program.cs
@@ -6,7 +6,41 @@ namespace variables_assignment_colter
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Welcome to C#");
+      int numberOfCupsOfCoffee = 1;
+      string fullName = "Colter Lena";
+      string today = "May 12, 2020";
+      var combinedVariables = $"{numberOfCupsOfCoffee}, {fullName}, and {today}";
+      Console.WriteLine (combinedVariables);
+
+      Console.WriteLine ("What is you name? ");
+      var userName = Console.ReadLine();
+
+      var greeting = $"It's a pleasure to meet you, {userName}.";
+      Console.WriteLine(greeting);
+
+      Console.WriteLine ("Please pick a number between 1 and 100. ");
+      var firstNumberAsString = Console.ReadLine();
+
+      double firstOperand = double.Parse(firstNumberAsString);
+
+      Console.WriteLine ("Please pick another number between 1 and 100. ");
+      var secondNumberAsString = Console.ReadLine();
+
+      double secondOperand = double.Parse(secondNumberAsString);
+
+      var sum = $"{firstOperand + secondOperand}";
+      
+      var difference = $"{firstOperand - secondOperand}";
+
+      var product = $"{firstOperand * secondOperand}";
+
+      var quotient = $"{firstOperand / secondOperand}";
+
+      var remainder = $"{firstOperand % secondOperand}";
+
+      var mathCalculations = $"The sum of {firstOperand} and {secondOperand} is {sum}. The difference of {firstOperand} and {secondOperand} is {difference}. The product of {firstOperand} and {secondOperand} is {product}. The quotient of {firstOperand} and {secondOperand} is {quotient}. The remainder when {firstOperand} is divided by {secondOperand} is {remainder}.";
+
+      Console.WriteLine (mathCalculations);
     }
   }
 }
